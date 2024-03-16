5955621cf06cf1a3ef61a303b4b05410d0e56ee5
isaacwach <isaacwachira57@gmail.com>
2024-03-16 11:39:06 +0300

    solved program one dart problem

---
+++ main.dart
@@ -0,0 +1,12 @@
+// PROGRAM 1
+
+void main() {
+  // Declare and initialize all the variables
+  String name = "Isaac";
+  int age = 23;
+  String school = "PLP academy";
+  String hobby = "Reading and coding";
+
+  // Print the user information
+  print("Your name is $name, you are $age years old, you school at $school, and you enjoy $hobby.");
+}
