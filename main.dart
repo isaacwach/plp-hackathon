ef17d1ebd49651c270eb15a331c4bd31a83542ed
isaacwach <isaacwachira57@gmail.com>
2024-03-16 11:51:43 +0300

    solved program 2 dart problem

--- main.dart
+++ main.dart
@@ -10,3 +10,29 @@ void main() {
   // Print the user information
   print("Your name is $name, you are $age years old, you school at $school, and you enjoy $hobby.");
 }
+
+
+// PROGRAM 2
+
+void opetations() {
+  double number1 = 12.5;
+  double number2 = 5;
+
+  //Perform addition operation
+  double sum = sum_of_numbers(number1, number2);
+  print(sum);
+
+  //Perform modulus operation
+  double remainder = remainder_of_numbes(number1, number2);
+  print("Remainder: $remainder");
+}
+
+// Function to add the two numbers
+double sum_of_numbers(double number1, double number2) {
+  return number1 + number2;
+}
+
+// Function to get the remainder
+double remainder_of_numbes(double number1, double number2) {
+  return number1 % number2;
+}
